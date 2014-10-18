8a28e47e4413a9f098237a49c8a388ea91b24708
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-18 09:39:58 +0200

    Determine which of two modules gets moved
    Also, rename all modules in test lists so that they read in a more
    natural order.

--- assay/tests.py
+++ assay/tests.py
@@ -1,40 +1,48 @@
-
 import unittest
 from .importation import partially_order
 
 class AssayTests(unittest.TestCase):
 
-    # We assume A imports B, that imports C, that imports D, et cetera.
+    # We assume that module B imports A, C imports B, D imports C, et
+    # cetera, while modules X and Y and Z are not part of the chain.
 
     def test_stability_when_nothing_is_wrong(self):
-        order = ['E', 'D', 'C', 'B', 'A']
+        order = ['A', 'B', 'C', 'D', 'E']
         edges = {}
         self.assertEqual(partially_order(order, edges),
-                         ['E', 'D', 'C', 'B', 'A'])
+                         ['A', 'B', 'C', 'D', 'E'])
 
     def test_simple_swap(self):
-        order = ['E', 'D', 'B', 'C', 'A']
-        edges = {'B': 'C'}
+        order = ['A', 'B', 'D', 'C', 'E']
+        edges = {'D': 'C'}
         self.assertEqual(partially_order(order, edges),
-                         ['E', 'D', 'C', 'B', 'A'])
+                         ['A', 'B', 'C', 'D', 'E'])
 
     def test_importing_main_module_first(self):
-        order = ['A', 'B', 'C', 'D', 'E']
-        edges = {'A': 'BCDE'}
+        order = ['E', 'D', 'C', 'B', 'A']
+        edges = {'E': 'ABCD'}
         self.assertEqual(partially_order(order, edges),
-                         ['B', 'C', 'D', 'E', 'A'])
+                         ['D', 'C', 'B', 'A', 'E'])
 
     def test_importing_middle_module_first(self):
-        order = ['C', 'D', 'E', 'B', 'A']
-        edges = {'C': 'DE'}
+        order = ['C', 'B', 'A', 'D', 'E']
+        edges = {'C': 'BA'}
         self.assertEqual(partially_order(order, edges),
-                         ['D', 'E', 'C', 'B', 'A'])
+                         ['B', 'A', 'C', 'D', 'E'])
 
     def test_having_two_module_swaps(self):
-        order = ['E', 'C', 'D', 'A', 'B']
-        edges = {'C': 'D', 'A': 'B'}
+        order = ['A', 'C', 'B', 'E', 'D']
+        edges = {'C': 'B', 'E': 'D'}
         self.assertEqual(partially_order(order, edges),
-                         ['E', 'D', 'C', 'B', 'A'])
+                         ['A', 'B', 'C', 'D', 'E'])
+
+    def test_which_module_moves(self):
+        # If module C is moved to the end of the list, then does C
+        # simply get moved back, or does D get moved to its right?
+        order = ['A', 'B', 'D', 'X', 'Y', 'Z', 'C']
+        edges = {'D': 'C'}
+        self.assertEqual(partially_order(order, edges),
+                         ['A', 'B', 'C', 'D', 'X', 'Y', 'Z'])
 
 if __name__ == '__main__':
     unittest.main()
