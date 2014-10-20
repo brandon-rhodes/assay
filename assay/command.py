315a22a91a5a1e4b2c3ca97ebf06b0dba3579446
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-20 14:19:49 -0400

    Fix worker-launch logic under Python 3

--- assay/command.py
+++ assay/command.py
@@ -1,22 +1,21 @@
 """Support direct invocation from the command line."""
 
-from __future__ import print_function
 import argparse
 import sys
 from .monitor import main_loop
 from .worker import TransformIntoWorker, worker_task
 
 def main():
+    sys.dont_write_bytecode = True
     parser = argparse.ArgumentParser(prog='assay')
     parser.description = 'Fast testing framework'
     parser.add_argument('name', nargs='+',
                         help='directory, package, or module to test')
     args = parser.parse_args()
-    sys.dont_write_bytecode = True
     try:
-        main_loop(args.module)
-    except TransformIntoWorker as pipes:
-        pass
+        main_loop(args.name)
+    except TransformIntoWorker as e:
+        pipes = e.args
     else:
         return
     worker_task(pipes)
