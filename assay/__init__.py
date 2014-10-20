2a2c0152cc66c1aece7fcce9e1d17078906f2784
Brandon Rhodes <brandon@rhodesmill.org>
2014-10-20 14:10:45 -0400

    Start a list of features in __init__.py

--- assay/__init__.py
+++ assay/__init__.py
@@ -1,4 +1,11 @@
-"""A fast Python testing framework."""
+"""A fast Python testing framework.
+
+* When Assay sees an orphan .pyc file without a matching .py file, it
+  reports an error.  This prevents the user from thinking that they have
+  successfully removed a module from their project when, in fact, their
+  tests are only passing because of the old .pyc file.
+
+"""
 
 import re
 from unittest.case import _AssertRaisesContext
