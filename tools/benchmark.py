f5e5f199b7e0d26b7582d6de8a4767c6f6f486dd
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-04 11:14:47 -0500

    Quick benchmark to know how expensive things are

---
+++ tools/benchmark.py
@@ -0,0 +1,42 @@
+from time import time
+from assay.worker import Worker, TransformIntoWorker, worker_task
+
+def dot():
+    return '.'
+
+def main():
+    worker = Worker()
+    n = 2000
+    items = [None] * n
+
+    t0 = time()
+    for item in items:
+        assert worker(int) == 0
+    dt = time() - t0
+
+    print('{:,.6f} s = {:,.1f} /s: Using a worker to call a built-in'
+          .format(dt / n, n / dt))
+
+    t0 = time()
+    for item in items:
+        assert worker(dot) == '.'
+    dt = time() - t0
+
+    print('{:,.6f} s = {:,.1f} /s: Using a worker to call a function'
+          .format(dt / n, n / dt))
+
+    t0 = time()
+    for item in items:
+        with worker:
+            assert worker(dot) == '.'
+    dt = time() - t0
+
+    print('{:,.6f} s = {:,.1f} /s: Pushing, calling, then popping a new worker'
+          .format(dt / n, n / dt))
+
+if __name__ == '__main__':
+    try:
+        main()
+    except TransformIntoWorker as e:
+        pipes = e.args
+        worker_task(pipes)
