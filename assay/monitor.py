dbe6825d5a41202629652b3eb726e30d7f9d6e36
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-07 16:14:01 -0500

    Cleanly close workers before restarting or exiting

--- assay/monitor.py
+++ assay/monitor.py
@@ -28,92 +28,96 @@ def main_loop(arguments, is_interactive):
     main_process_paths = set(path for name, path in list_module_paths())
     file_watcher = Filesystem()
 
-    while True:
-        # import_order = improve_order(import_order, dangers)
-        # print('Importing {}'.format(module_names))
-        t0 = time()
-        with contextlib.nested(*workers):
-            names = []
-            for item in items:
-                import_path, import_name = item
-                more_names = search_argument(import_path, import_name)
-                names.extend(more_names)
-            # t0 = time()
-            # module_paths, events = worker(import_modules, import_order)
-            # pprint(events)
-            # print('  {} seconds'.format(time() - t0))
-            # print()
+    try:
+        while True:
+            # import_order = improve_order(import_order, dangers)
+            # print('Importing {}'.format(module_names))
+            t0 = time()
+            with contextlib.nested(*workers):
+                names = []
+                for item in items:
+                    import_path, import_name = item
+                    more_names = search_argument(import_path, import_name)
+                    names.extend(more_names)
+                # t0 = time()
+                # module_paths, events = worker(import_modules, import_order)
+                # pprint(events)
+                # print('  {} seconds'.format(time() - t0))
+                # print()
 
-            successes = failures = 0
-            worker_fds = {w.fileno(): w for w in workers}
+                successes = failures = 0
+                worker_fds = {w.fileno(): w for w in workers}
 
-            poller = select.epoll()
-            if is_interactive:
-                poller.register(sys.stdin, select.EPOLLIN)
-            for w in workers:
-                if names:
-                    name = names.pop()
-                    w.start(capture_stdout_stderr, run_tests_of, name)
-                    # w.start(run_tests_of, name)
-                    poller.register(w, select.EPOLLIN)
-            while worker_fds:
-                for fd, flags in poller.poll():
-                    w = worker_fds.get(fd)
-                    if w is None:
-                        for keystroke in sys.stdin.read():
-                            print('got {}'.format(keystroke))
-                            if keystroke == 'q':
-                                sys.exit(1)
-                            elif keystroke == 'r':
-                                raise Restart()
-                        continue
-                    result = w.next()
-                    if result is StopIteration:
-                        if names:
-                            name = names.pop()
-                            w.start(capture_stdout_stderr, run_tests_of, name)
-                            # w.start(run_tests_of, name)
+                poller = select.epoll()
+                if is_interactive:
+                    poller.register(sys.stdin, select.EPOLLIN)
+                for w in workers:
+                    if names:
+                        name = names.pop()
+                        w.start(capture_stdout_stderr, run_tests_of, name)
+                        # w.start(run_tests_of, name)
+                        poller.register(w, select.EPOLLIN)
+                while worker_fds:
+                    for fd, flags in poller.poll():
+                        w = worker_fds.get(fd)
+                        if w is None:
+                            for keystroke in sys.stdin.read():
+                                print('got {}'.format(keystroke))
+                                if keystroke == 'q':
+                                    sys.exit()
+                                elif keystroke == 'r':
+                                    raise Restart()
+                            continue
+                        result = w.next()
+                        if result is StopIteration:
+                            if names:
+                                name = names.pop()
+                                w.start(capture_stdout_stderr, run_tests_of, name)
+                                # w.start(run_tests_of, name)
+                            else:
+                                poller.unregister(w)
+                                del worker_fds[fd]
+                        elif isinstance(result, str):
+                            write('.')
+                            flush()
+                            successes += 1
                         else:
-                            poller.unregister(w)
-                            del worker_fds[fd]
-                    elif isinstance(result, str):
-                        write('.')
-                        flush()
-                        successes += 1
-                    else:
-                        pretty_print_exception(*result)
-                        flush()
-                        failures += 1
-            paths = [path for name_, path in worker.call(list_module_paths)]
-        print()
-        dt = time() - t0
-        if failures:
-            tally = red('{} of {} tests failed'.format(
-                failures, successes + failures))
-        else:
-            tally = green('All {} tests passed'.format(successes))
-        print('{} in {:.2f} seconds'.format(tally, dt))
-
-        if not sys.stdout.isatty():
-            break
-
-        print('Watching', len(paths), 'paths', end='...')
-        flush()
-        file_watcher.add_paths(paths)
-        changes = file_watcher.wait()
-        paths = [os.path.join(directory, filename)
-                 for directory, filename in changes]
-
-        main_process_changes = main_process_paths.intersection(paths)
-        if main_process_changes:
-            example_path = main_process_changes.pop()
+                            pretty_print_exception(*result)
+                            flush()
+                            failures += 1
+                paths = [path for name_, path in worker.call(list_module_paths)]
             print()
-            print('Detected edit to {}'.format(example_path))
-            for w in workers:
-                w.close()
-            raise Restart()
-        print()
-        print('Running tests')
+            dt = time() - t0
+            if failures:
+                tally = red('{} of {} tests failed'.format(
+                    failures, successes + failures))
+            else:
+                tally = green('All {} tests passed'.format(successes))
+            print('{} in {:.2f} seconds'.format(tally, dt))
+
+            if not sys.stdout.isatty():
+                break
+
+            print('Watching', len(paths), 'paths', end='...')
+            flush()
+            file_watcher.add_paths(paths)
+            changes = file_watcher.wait()
+            paths = [os.path.join(directory, filename)
+                     for directory, filename in changes]
+
+            main_process_changes = main_process_paths.intersection(paths)
+            if main_process_changes:
+                example_path = main_process_changes.pop()
+                print()
+                print('Detected edit to {}'.format(example_path))
+                for w in workers:
+                    w.close()
+                raise Restart()
+            print()
+            print('Running tests')
+    finally:
+        for w in workers:
+            w.close()
 
 def speculatively_import_then_loop(import_order, ):
     pass
