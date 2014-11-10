58ad21ac4e4f81fd3102822d88a4eec67133abb3
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-09 23:42:13 -0500

    An experiment: using datagrams to provide framing
    Unfortunately this seems to negatively impact the speed at which worker
    communication can occur, which would therefore slow the rate at which
    Assay could run tests.

--- assay/worker.py
+++ assay/worker.py
@@ -1,10 +1,12 @@
 """A worker process that can respond to commands."""
 
 import os
+import socket
 import sys
 import unix
 from types import GeneratorType
 
+_enough = 1024 * 1024
 _python3 = (sys.version_info.major >= 3)
 
 if _python3:
@@ -16,24 +18,20 @@ class Worker(object):
     """An object in the main process for communicating with one worker."""
 
     def __init__(self):
-        from_parent, to_child = os.pipe()
-        from_child, to_parent = os.pipe()
+        sock, child_sock = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM)
 
-        unix.close_on_exec(to_child)
-        unix.close_on_exec(from_child)
-
+        unix.close_on_exec(sock.fileno())
         child_pid = os.fork()
         if not child_pid:
+            python = sys.executable
+            n = str(child_sock.fileno())
             os.setpgrp()  # prevent worker from receiving Ctrl-C
-            os.execvp(sys.executable, [sys.executable, '-m', 'assay.worker',
-                                       str(to_parent), str(from_parent)])
+            os.execvp(python, [python, '-m', 'assay.worker', n])
 
-        os.close(to_parent)
-        os.close(from_parent)
+        child_sock.close()
 
         self.pids = [child_pid]
-        self.to_child = os.fdopen(to_child, 'wb')
-        self.from_child = os.fdopen(from_child, 'rb', 0)
+        self.sock = sock
 
     def push(self):
         """Have the worker push a new subprocess on top of the stack."""
@@ -46,22 +44,20 @@ class Worker(object):
 
     def call(self, function, *args, **kw):
         """Run a function in the worker process and return its result."""
-        pickle.dump((function, args, kw), self.to_child)
-        self.to_child.flush()
-        return pickle.load(self.from_child)
+        self.sock.send(pickle.dumps((function, args, kw)))
+        return pickle.loads(self.sock.recv(_enough))
 
     def start(self, generator, *args, **kw):
         """Start a generator in the worker process."""
-        pickle.dump((generator, args, kw), self.to_child)
-        self.to_child.flush()
+        self.sock.send(pickle.dumps((generator, args, kw)))
 
     def next(self):
         """Return the next item from the generator given to `start()`."""
-        return pickle.load(self.from_child)
+        return pickle.loads(self.sock.recv(_enough))
 
     def fileno(self):
         """Return the incoming file descriptor, for `epoll()` objects."""
-        return self.from_child.fileno()
+        return self.sock.fileno()
 
     def __enter__(self):
         """During a 'with' statement, run commands in a clone of the worker."""
@@ -75,8 +71,7 @@ class Worker(object):
         """Kill the worker and close our file descriptors."""
         while self.pids:
             unix.kill_dash_9(self.pids.pop())
-        self.to_child.close()
-        self.from_child.close()
+        self.sock.close()
 
 def push():
     """Fork a child worker, who will own the pipe until it exits."""
@@ -86,31 +81,27 @@ def push():
     os.waitpid(child_pid, 0)
     return 'worker process popped'
 
-def worker_process(to_parent, from_parent):
+def worker_process(sock_fd):
     """Run functions piped to us from the parent process.
 
     Both `to_parent` and `from_parent` should be integer file
     descriptors of the pipes connecting us to the parent process.
 
     """
-    to_parent = os.fdopen(to_parent, 'wb')
-    from_parent = os.fdopen(from_parent, 'rb')
+    sock = socket.fromfd(sock_fd, socket.AF_UNIX, socket.SOCK_DGRAM)
 
     while True:
-        function, args, kw = pickle.load(from_parent)
+        function, args, kw = pickle.loads(sock.recv(_enough))
         result = function(*args, **kw)
         if isinstance(result, GeneratorType):
             for item in result:
-                pickle.dump(item, to_parent, 2)
-                to_parent.flush()
-            pickle.dump(StopIteration, to_parent, 2)
-            to_parent.flush()
+                sock.send(pickle.dumps(item, 2))
+            sock.send(pickle.dumps(StopIteration, 2))
         else:
-            pickle.dump(result, to_parent, 2)
-            to_parent.flush()
+            sock.send(pickle.dumps(result, 2))
 
 if __name__ == '__main__':
     try:
-        worker_process(int(sys.argv[1]), int(sys.argv[2]))
+        worker_process(int(sys.argv[1]))
     except KeyboardInterrupt:
         pass
