406081f277363cd134ceaf9c63b48a3ef80e9cd2
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-10 21:44:16 -0500

    Revert "An experiment: using datagrams to provide framing"
    This reverts commit 58ad21ac4e4f81fd3102822d88a4eec67133abb3.
    
    Wow, datagrams were slow.  They cost nearly twice as much as writes and
    reads to plain pipes, even despite the fact that such operations require
    buffering to work.
    
    Conflicts:
    	assay/worker.py

--- assay/worker.py
+++ assay/worker.py
@@ -1,12 +1,10 @@
 """A worker process that can respond to commands."""
 
 import os
-import socket
 import sys
 import unix
 from types import GeneratorType
 
-_enough = 1024 * 1024
 _python3 = (sys.version_info.major >= 3)
 
 if _python3:
@@ -18,26 +16,24 @@ class Worker(object):
     """An object in the main process for communicating with one worker."""
 
     def __init__(self):
-        sock, child_sock = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM)
+        from_parent, to_child = os.pipe()
+        from_child, to_parent = os.pipe()
 
-        unix.close_on_exec(sock.fileno())
+        unix.close_on_exec(to_child)
+        unix.close_on_exec(from_child)
+
         child_pid = os.fork()
         if not child_pid:
-            python = sys.executable
-            n = str(child_sock.fileno())
             os.setpgrp()  # prevent worker from receiving Ctrl-C
-            os.execvp(python, [python, '-m', 'assay.worker', n])
+            os.execvp(sys.executable, [sys.executable, '-m', 'assay.worker',
+                                       str(to_parent), str(from_parent)])
 
-        child_sock.close()
+        os.close(to_parent)
+        os.close(from_parent)
 
         self.pids = [child_pid]
-        self.sock = sock
-
-    def _send(self, obj):
-        self.sock.send(pickle.dumps(obj))
-
-    def _recv(self):
-        return pickle.loads(self.sock.recv(_enough))
+        self.to_child = os.fdopen(to_child, 'wb')
+        self.from_child = os.fdopen(from_child, 'rb', 0)
 
     def push(self):
         """Have the worker push a new subprocess on top of the stack."""
@@ -46,7 +42,7 @@ class Worker(object):
     def pop(self):
         """Kill the top subprocess and pop it from the stack."""
         unix.kill_dash_9(self.pids.pop())
-        assert self._recv() == self.pids[-1]
+        assert pickle.load(self.from_child) == self.pids[-1]
         # sock = self.sock
         # sock.setblocking(False)
         # while sock.recv():
@@ -55,20 +51,22 @@ class Worker(object):
 
     def call(self, function, *args, **kw):
         """Run a function in the worker process and return its result."""
-        self._send((function, args, kw))
-        return self._recv()
+        pickle.dump((function, args, kw), self.to_child)
+        self.to_child.flush()
+        return pickle.load(self.from_child)
 
     def start(self, generator, *args, **kw):
         """Start a generator in the worker process."""
-        self._send((generator, args, kw))
+        pickle.dump((generator, args, kw), self.to_child)
+        self.to_child.flush()
 
     def next(self):
         """Return the next item from the generator given to `start()`."""
-        return self._recv()
+        return pickle.load(self.from_child)
 
     def fileno(self):
         """Return the incoming file descriptor, for `epoll()` objects."""
-        return self.sock.fileno()
+        return self.from_child.fileno()
 
     def __enter__(self):
         """During a 'with' statement, run commands in a clone of the worker."""
@@ -82,32 +80,48 @@ class Worker(object):
         """Kill the worker and close our file descriptors."""
         while self.pids:
             unix.kill_dash_9(self.pids.pop())
-        self.sock.close()
+        self.to_child.close()
+        self.from_child.close()
 
-def worker_process(sock_fd):
+def push():
+    """Fork a child worker, who will own the pipe until it exits."""
+    child_pid = os.fork()
+    if not child_pid:
+        return os.getpid()
+    os.waitpid(child_pid, 0)
+    return 'worker process popped'
+
+def worker_process(to_parent, from_parent):
     """Run functions piped to us from the parent process.
 
     Both `to_parent` and `from_parent` should be integer file
     descriptors of the pipes connecting us to the parent process.
 
     """
-    sock = socket.fromfd(sock_fd, socket.AF_UNIX, socket.SOCK_DGRAM)
+    to_parent = os.fdopen(to_parent, 'wb')
+    from_parent = os.fdopen(from_parent, 'rb')
 
     while True:
-        function, args, kw = pickle.loads(sock.recv(_enough))
+        function, args, kw = pickle.load(from_parent)
         result = function(*args, **kw)
         if function is os.fork:
             if result:
                 os.waitpid(result, 0)
             result = os.getpid()
+            pickle.dump(result, to_parent, 2)
+            to_parent.flush()
         elif isinstance(result, GeneratorType):
             for item in result:
-                sock.send(pickle.dumps(item, 2))
-            result = StopIteration
-        sock.send(pickle.dumps(result, 2))
+                pickle.dump(item, to_parent, 2)
+                to_parent.flush()
+            pickle.dump(StopIteration, to_parent, 2)
+            to_parent.flush()
+        else:
+            pickle.dump(result, to_parent, 2)
+            to_parent.flush()
 
 if __name__ == '__main__':
     try:
-        worker_process(int(sys.argv[1]))
+        worker_process(int(sys.argv[1]), int(sys.argv[2]))
     except KeyboardInterrupt:
         pass
