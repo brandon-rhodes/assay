579fc6c2a4b89981b5500ba35f4cdb18933f5ff6
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-10 22:49:19 -0500

    Clarifying comments to remember important lessons

--- assay/worker.py
+++ assay/worker.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-import unix
+from . import unix
 from types import GeneratorType
 
 _python3 = (sys.version_info.major >= 3)
@@ -11,6 +11,11 @@ if _python3:
     import pickle
 else:
     import cPickle as pickle
+
+# A crucial setting!  If we were to allow buffering of data from the
+# worker, then the buffer might read ahead on the input stream and leave
+# the descriptor looking empty from the point of view of epoll().
+FROM_WORKER_BUFSIZE = 0
 
 class Worker(object):
     """An object in the main process for communicating with one worker."""
@@ -37,7 +42,7 @@ class Worker(object):
 
         self.pids = [worker_pid]
         self.to_worker = os.fdopen(to_worker, 'wb')
-        self.from_worker = os.fdopen(from_worker, 'rb', 0)
+        self.from_worker = os.fdopen(from_worker, 'rb', FROM_WORKER_BUFSIZE)
         self.sync_from_worker = sync_from_worker
 
     def push(self):
@@ -45,7 +50,14 @@ class Worker(object):
         self.pids.append(self.call(os.fork))
 
     def pop(self):
-        """Kill the top subprocess and pop it from the stack."""
+        """Kill the active worker subprocess and pop it from the stack.
+
+        Because we might happen to kill a worker as it is in the middle
+        of writing out a pickle, we listen on the separate "sync" pipe
+        for the worker's parent to confirm that the worker has exited.
+        Then we discard any left-over bytes on the data pipe.
+
+        """
         unix.kill_dash_9(self.pids.pop())
         os.read(self.sync_from_worker, 1)
         unix.discard_input(self.from_worker.fileno())
@@ -83,6 +95,7 @@ class Worker(object):
             unix.kill_dash_9(self.pids.pop())
         self.to_worker.close()
         self.from_worker.close()
+        os.close(self.sync_from_worker)
 
 def worker_process(from_parent, to_parent, sync_to_parent):
     """Run functions piped to us from the parent process.
