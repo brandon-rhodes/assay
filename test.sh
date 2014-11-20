ec11fc66e5d9beef3c7089eb573361089e9014e1
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-20 12:07:29 -0500

    Silence "coverage" about test files that go away

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd $(dirname "$0")
-PYTHONPATH=. coverage-2.7 run -m assay.tests && coverage html
+PYTHONPATH=. coverage-2.7 run -m assay.tests && coverage html -i
 echo '==========='
 PYTHONPATH=. python3.4 -m assay.tests
