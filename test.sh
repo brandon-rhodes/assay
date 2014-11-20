cc8bf0106e358a6a81b4d6cb02d884017b58eaf7
Brandon Rhodes <brandon@rhodesmill.org>
2014-11-20 10:33:45 -0500

    Rebuild coverage output on each test run

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd $(dirname "$0")
-PYTHONPATH=. python2.7 -m assay.tests
+PYTHONPATH=. coverage-2.7 run -m assay.tests && coverage html
 echo '==========='
 PYTHONPATH=. python3.4 -m assay.tests
